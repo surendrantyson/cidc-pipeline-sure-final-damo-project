cdded6004146ed9976ac50477834e9c7e3fa0677
Surendran C <surendrantyson.c@gmail.com>
2023-05-08 17:29:59 +0530

    Html code change suren colourfulll form
    Html code change suren colourfulll form

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-#uvicorn main:app --reload
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<body>
+<h1>Suren project done! happy</h1>
+<h2>A basic HTML table</h2>
 
-app = FastAPI()
-templates = Jinja2Templates(directory="/code")
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Project Tools</td>
+    <td>Jenkins</td>
+    <td>Maven Build</td>
+  </tr>
+  <tr>
+    <td>Project Instance</td>
+    <td>Ubuntu</td>
+    <td>Ubuntu</td>
+  </tr>
+</table>
+ <h1 style="background-color:Tomato;">Tomato</h1>
+<h1 style="background-color:Orange;">Orange</h1>
+<h1 style="background-color:DodgerBlue;">DodgerBlue</h1>
+<h1 style="background-color:MediumSeaGreen;">MediumSeaGreen</h1>
+<h1 style="background-color:Gray;">Gray</h1>
+<h1 style="background-color:SlateBlue;">SlateBlue</h1>
+<h1 style="background-color:Violet;">Violet</h1>
+<h1 style="background-color:LightGray;">LightGray</h1>
+ 
 
-@app.get("/")
-def form_post(request: Request):
-    return templates.TemplateResponse('form.html', context={'request': request})
+<p>To understand the example better, I have added sample HTML codes Thank you!.</p>
 
-
+</body>
+</html>
